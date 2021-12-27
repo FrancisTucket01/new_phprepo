706f6b3607f53ff9948d084379d57c15b5ede243
Francis Mwazala <francismwazala01@gmail.com>
2021-12-27 10:45:50 +0300

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,14 +1,14 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "UtGp1ssC6O";
-
-// $servername = "remotemysql.com";
-// $username = "UtGp1ssC6O";
-// $password = "2e3pUyEXs9";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
 // $dbname = "UtGp1ssC6O";
+
+$servername = "remotemysql.com";
+$username = "UtGp1ssC6O";
+$password = "2e3pUyEXs9";
+$dbname = "UtGp1ssC6O";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
